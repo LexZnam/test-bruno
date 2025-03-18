19867d97e76eaba99037ef02607b55e2aa6f9aec
AZnamenski <Ocelot87@mail.ru>
2025-03-18 18:30:06 +0300

    master 4
    branch1 1
    
    master 3
    
    branch3 2
    
    branch2 1
    
    master 1  ch 1

--- Main.java
+++ Main.java
@@ -3,7 +3,16 @@ package com.Brunoyam;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Ветка мастер");
+        System.out.println("Ветка мастер + ветка 2");
+        System.out.println("в1");
+        System.out.println("Ветка 1");
+        System.out.println("Ветка 3");
+        System.out.println("изм 1");
+        System.out.println("dop 1");
+        System.out.println("изм 2");
+        System.out.println("rep1");
+        System.out.println("rep2");
+
 
     }
 }
